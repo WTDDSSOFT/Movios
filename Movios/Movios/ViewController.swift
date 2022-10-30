b228453f21a5236203a522dab52b1e46c3108d53
William Torres Dias Dos Santos <58994739+WTDDSSOFT@users.noreply.github.com>
2022-10-29 23:01:09 -0400

    started movios app

---
+++ Movios/Movios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movios
+//
+//  Created by william torres dias dos santos on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
---
+++ MoviosUi/MoviosUi/MoviosUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviosUiApp.swift
+//  MoviosUi
+//
+//  Created by william torres dias dos santos on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviosUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
